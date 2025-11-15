d3c3ebda241734f3a6e39132c83e0585e523fff8
Anam parwez <anamparwez18@gmail.com>
2025-11-15 20:21:44 +0530

    new file

--- server.js
+++ server.js
@@ -1,35 +1,4 @@
 
-const http = require('http');
-const fs=require('fs');
-const querystring=require('querystring');
-http.createServer((req,resp)=>{
-  fs.readFile('html/form.html','utf-8',(error,data)=>{
-        
-      if(error){
-         resp.writeHead(500,{"content-type" : 'text/plain'})
-          resp.end('Internal server error')
-           return;
-        }
-        if(req.url=='/'){
-     resp.write(data);
-        }
-        else if(req.url=='/submit'){
-          let dataBody=[];
-          req.on('data',(chunk)=>{ //data is stored in nodejs in form of  chunks
-            dataBody.push(chunk);
-
-          });
-          req.on('end',()=>{
-            let rowdata=Buffer.concat(dataBody).toString();
-            let readableData=querystring.parse(rowdata);
-            console.log(readableData);
-          })
-            resp.write('Form submitted')
-        }
-         resp.end();
-    })
-   
-}).listen(3200)
 
 //const http= require('http');
 //h//ttp.createServer((req,resp)=>{
